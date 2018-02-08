dffad6cdbd6cbad4a2e79d1451ed25cd3bf30b97
Matt Price <matt.price@utoronto.ca>
2018-02-08 08:56:35 -0500

    fix reflection tests to look for the right file

--- test/test-pt3.js
+++ test/test-pt3.js
@@ -6,17 +6,18 @@ var chai=require('chai'),
     expect=chai.expect;
 chai.use(require('chai-fs'));
 
-var githubid;
+// var githubid;
 
-gitConfig(function (err, config) {
-  if (err) return done(err);
-  if (config.github.user) {githubid = config.github.user;}
+// gitConfig(function (err, config) {
+//   if (err) return done(err);
+//   if (config.github.user) {githubid = config.github.user;}
   
-});
+// });
 
 describe('Reflection Checks (not required unless you are attempting an "A" grade!)', function() {
   it('Reflection file should exist', function() {
-    let r = `Reflection/${githubid}.md`;
+    //let r = `Reflection/${githubid}.md`;
+    let r = `Reflection/reflection.md`;
     expect(r, `I can't find the file ${r}`).to.be.a.file();
   });
   it('The total word count for your reflection should be at least 625', function() {
