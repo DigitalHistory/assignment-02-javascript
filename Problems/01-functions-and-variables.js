de80b8e0c0d19276d63d39c2cca79290521415f1
Matt Price <matt.price@utoronto.ca>
2019-09-25 22:26:12 -0400

    problem 1: cleanup
    - var --> let
    - add jsdoc comments to functions

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -2,12 +2,12 @@
 // to functions (as parameters) and from functions (as return values)
 
 // in JS we work with several kinds of variables.
-// There are several ways to declare varaiables, but we will always use
+// There are several ways to declare varaiables, but we will generally use
 // this structure:
-// var name = value;
+// let name = value;
 
 // you can also declare several variables at once:
-// var name1 = value1,
+// let name1 = value1,
 //     name2 = value2,
 //     name3 = value3;
 // note the commas on the first 2 lines, and the semi-colon on the last
@@ -21,10 +21,15 @@
 // Objects are quite a bit more complex than the other variable types and we will
 // discuss them later.
 
+// the special comment blocks above each function should help VSCode correct your mistakes. 
 
 // Problem 1
 // write a function "hello" that always returns the string "Hello!"
 
+/**
+ * greet the user!
+ * @returns {string} should be exactly "Hello!"
+ */
 function hello () {
   // if you want you can declare a string variable first
   // or in this case you can just return the string directly
@@ -35,18 +40,29 @@ function hello () {
 // Improve your initial function by accepting a parameter
 // "name" and returning a string "Hello, [insert name]!"
 
+/**
+ * create a string "Hello, NAME!",where NAME is the  parameter value
+ * @param {string} name
+ * @returns {string} a sentence that uses NAME i nits construction.
+ */
 function greeting(name) {
   // use the '+' operator to combine
   // strings.  So for instance, if I wanted to
   // say goodbye, I might write
-  // var parting = "Goodbye, " + name + ".";
+  // let parting = "Goodbye, " + name + ".";
   // (but of course, I actually want to say hello.)
 }
 
 // Problem 3
 // write a function that takes three parameters and returns an array
 // consisting of the three parameters together
-
+/**
+ * creat an array from three inputs FIRST, SECOND, and THIRD
+ * @param {} first
+ * @param {} second
+ * @param {} third
+ * @returns {array} 
+ */
 function returnArray (first, second, third) {
   // you can define the array using "new Array ()" or just "[ , , ]"
   // don't forget to return it
@@ -60,6 +76,12 @@ function returnArray (first, second, third) {
 // should return
 // "Italo Calvino was a novelist."
 
+
+/**
+ * split THISARRAY and use its elements to compose a sentence
+ * @param {} thisArray of minimum length 3
+ * @returns {string} a sentence  composed from the first 3 elements of the array
+ */
 function splitArray (thisArray) {
   // remember you can access individual elements of an array with
   // array[0]
@@ -71,6 +93,12 @@ function splitArray (thisArray) {
 // Problem 5
 // A. Write a simple function that takes two arguments.  Subtact the second number from the first.
 // Return the result
+/**
+ * subtract NUMBER2 from NUMBER1
+ * @param {number} number1
+ * @param {number} number2
+ * @returns {number} the difference of the two parameters
+ */
 function subtract(number1, number2) {
   // subtract number2 from number1, and return the result.
 }
@@ -95,6 +123,11 @@ function carefulSubtract (first, second) {
 // if the parameter is a number, return the *square* of the number
 // if the parameter is null, return the string "sorry, I can't do anything with a null value."
 // for any other type, return "I don't know how to use that kind of variable."
+/**
+ * identify the type of UNKNOWN
+ * @param {} unknown
+ * @returns {} a meessage about unknown, or the square of unknown if it is a number
+ */
 function typeTester (unknown) {
   // use an if/else construction, a switch/case , or any other branching logic. Remember to
   // return a value. 
@@ -105,7 +138,7 @@ function typeTester (unknown) {
 // DO NOT MODIFY -- FOR AUTOMATED TESTING ONLY
 // MODIFYING THIS CODE WILL ALMOST CERTAINLY CAUSE YOUR TESTS TO BREAK
 // AND YOUR ASSIGNMENT TO FAIL!
-var exports;
+let exports;
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   exports = module.exports = {};
